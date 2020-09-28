2a613c6ef962bc0b6851425b7aae86d15bf4472e
Ngonyoku <ngonyoku001@gmail.com>
2020-09-28 12:47:18 +0300

    Robot can now be drawn

--- robot_builder.py
+++ robot_builder.py
@@ -18,50 +18,43 @@ def rectangle(horizontal, vertical, color):
     t.speed('slow')  # set's the turtle speed
     t.bgcolor('Dodger blue')  # Makes the background of the window dodge blue
 
-    # Feet
-    t.goto(-100, -150)  # Move the turtle position x = -100 and y = 150
-    rectangle(50, 20, 'blue')  # We use the rectangle function to draw a blue rect 50 wide and 20 high
-    t.goto(-30, - 150)
-    rectangle(50, 20, 'blue')
 
-    # legs
-    t.goto(-25, -50)  # the turtle moves to position x = -25, y = 50
-    rectangle(15, 100, 'grey')  # Draws the left leg
-    t.goto(-55, -50)
-    rectangle(-15, 100, 'grey')  # Draws the Right leg
-
-    # body
-    t.goto(-90, 100)
-    rectangle(100, 150, 'red')
-
-    # arms
-    t.goto(-150, 70)
-    rectangle(60, 15, 'grey')  # Upper right arm
-    t.goto(-150, 110)
-    rectangle(15, 40, 'grey')  # lower right arm
-
-    t.goto(10, 70)
-    rectangle(60, 15, 'grey')  # Upper left arm
-    t.goto(55, 110)
-    rectangle(15, 40, 'grey')  # Lower left arm
-
-    # Neck
-    t.goto(-50, 120)
-    rectangle(15, 20, 'grey')
-
-    # Head
-    t.goto(-85, 170)
-    rectangle(80, 50, 'red')
-
-    # Eyes
-    t.goto(-60, 160)
-    rectangle(30, 10, 'white')  # White part of the eye
-    t.goto(-55, 155)
-    rectangle(5, 5, 'black')  # Draw the Right Pupil
-    t.goto(-40, 155)
-    rectangle(5, 5, 'black')  # Draw the left pupil
-
-    # Mouth
-    t.goto(-65, 135)
-    rectangle(40, 5, 'black')
-    t.hideturtle()  # This will make the turtle invisible
+# Feet
+t.goto(-100, -150)  # Move the turtle position x = -100 and y = 150
+rectangle(50, 20, 'blue')  # We use the rectangle function to draw a blue rect 50 wide and 20 high
+t.goto(-30, - 150)
+rectangle(50, 20, 'blue')
+# legs
+t.goto(-25, -50)  # the turtle moves to position x = -25, y = 50
+rectangle(15, 100, 'grey')  # Draws the left leg
+t.goto(-55, -50)
+rectangle(-15, 100, 'grey')  # Draws the Right leg
+# body
+t.goto(-90, 100)
+rectangle(100, 150, 'red')
+# arms
+t.goto(-150, 70)
+rectangle(60, 15, 'grey')  # Upper right arm
+t.goto(-150, 110)
+rectangle(15, 40, 'grey')  # lower right arm
+t.goto(10, 70)
+rectangle(60, 15, 'grey')  # Upper left arm
+t.goto(55, 110)
+rectangle(15, 40, 'grey')  # Lower left arm
+# Neck
+t.goto(-50, 120)
+rectangle(15, 20, 'grey')
+# Head
+t.goto(-85, 170)
+rectangle(80, 50, 'red')
+# Eyes
+t.goto(-60, 160)
+rectangle(30, 10, 'white')  # White part of the eye
+t.goto(-55, 155)
+rectangle(5, 5, 'black')  # Draw the Right Pupil
+t.goto(-40, 155)
+rectangle(5, 5, 'black')  # Draw the left pupil
+# Mouth
+t.goto(-65, 135)
+rectangle(40, 5, 'black')
+t.hideturtle()  # This will make the turtle invisible
